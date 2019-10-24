ca93cd3d2dcadb15f149e154885567c4f6ae0bfa
Dev <shayan.dev98@gmail.com>
2019-10-24 05:48:46 +0530

    tried using export

---
+++ cloudinaryVar.js
@@ -0,0 +1,7 @@
+const donenv = require('dotenv/config')
+
+
+module.exports = {
+ CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
+ CLOUDINARY_URL: process.env.CLOUDINARY_URL
+}
--- public/script.js
+++ public/script.js
@@ -1,27 +1,32 @@
+var cloudab = require('../cloudinaryVar.js')
 
-const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/shayan-dev/image/upload';
-const CLOUDINARY_UPLOAD_PRESET = 'fojw1igo';
-//alert('yelo');
+
+//const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/shayan-dev/image/upload';
+//const CLOUDINARY_UPLOAD_PRESET = 'fojw1igo';
+
+
 //console.log(process.env.CLOUDINARY_UPLOAD_PRESET);
 
+alert("helo");
 const image = document.querySelector('#pan-File');
 image.addEventListener('change', (e) => {
-  const file = e.target.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
-  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+ const file = e.target.files[0];
+ const formData = new FormData();
+ formData.append('file', file);
+ formData.append('upload_preset', cloudab.CLOUDINARY_UPLOAD_PRESET);
 
-  //alert('hello');
-  fetch(CLOUDINARY_URL, {
-    method: 'POST',
-    body: formData,
+
+
+ fetch(cloudab.CLOUDINARY_URL, {
+  method: 'POST',
+  body: formData,
+ })
+  .then(response => response.json())
+  .then((data) => {
+   if (data.secure_url !== '') {
+    const uploadedFileUrl = data.secure_url;
+    localStorage.setItem('passportUrl', uploadedFileUrl);
+   }
   })
-    .then(response => response.json())
-    .then((data) => {
-      if (data.secure_url !== '') {
-        const uploadedFileUrl = data.secure_url;
-        localStorage.setItem('passportUrl', uploadedFileUrl);
-      }
-    })
-    .catch(err => console.error(err));
+  .catch(err => console.error(err));
 });
